4b16c1e7c975f689ccd34496735dbd44ac4912a9
Andee Kaplan <andee.kaplan@gmail.com>
2018-06-08 13:51:50 -0400

    screen thingie

--- code/record_linkage/run_jasa_eber.sh
+++ code/record_linkage/run_jasa_eber.sh
@@ -1,12 +1,12 @@
-screen -d -m -S rl-5-5 sh 'Rscript code/record_linkage/jasa_eber.R 5 5'
-screen -d -m -S rl-5-10 sh 'Rscript code/record_linkage/jasa_eber.R 5 10'
-screen -d -m -S rl-5-15 sh 'Rscript code/record_linkage/jasa_eber.R 5 15'
-screen -d -m -S rl-10-5 sh 'Rscript code/record_linkage/jasa_eber.R 10 5'
-screen -d -m -S rl-10-10 sh 'Rscript code/record_linkage/jasa_eber.R 10 10'
-screen -d -m -S rl-10-15 sh 'Rscript code/record_linkage/jasa_eber.R 10 15'
-screen -d -m -S rl-30-5 sh 'Rscript code/record_linkage/jasa_eber.R 30 5'
-screen -d -m -S rl-30-10 sh 'Rscript code/record_linkage/jasa_eber.R 30 10'
-screen -d -m -S rl-30-15 sh 'Rscript code/record_linkage/jasa_eber.R 30 15'
-screen -d -m -S rl-50-5 sh 'Rscript code/record_linkage/jasa_eber.R 50 5'
-screen -d -m -S rl-50-10 sh 'Rscript code/record_linkage/jasa_eber.R 50 10'
-screen -d -m -S rl-50-15 sh 'Rscript code/record_linkage/jasa_eber.R 50 15'
+screen -d -m -S rl-5-5 Rscript code/record_linkage/jasa_eber.R 5 5
+screen -d -m -S rl-5-10 Rscript code/record_linkage/jasa_eber.R 5 10
+screen -d -m -S rl-5-15 Rscript code/record_linkage/jasa_eber.R 5 15
+screen -d -m -S rl-10-5 Rscript code/record_linkage/jasa_eber.R 10 5
+screen -d -m -S rl-10-10 Rscript code/record_linkage/jasa_eber.R 10 10
+screen -d -m -S rl-10-15 Rscript code/record_linkage/jasa_eber.R 10 15
+screen -d -m -S rl-30-5 Rscript code/record_linkage/jasa_eber.R 30 5
+screen -d -m -S rl-30-10 Rscript code/record_linkage/jasa_eber.R 30 10
+screen -d -m -S rl-30-15 Rscript code/record_linkage/jasa_eber.R 30 15
+screen -d -m -S rl-50-5 Rscript code/record_linkage/jasa_eber.R 50 5
+screen -d -m -S rl-50-10 Rscript code/record_linkage/jasa_eber.R 50 10
+screen -d -m -S rl-50-15 Rscript code/record_linkage/jasa_eber.R 50 15
