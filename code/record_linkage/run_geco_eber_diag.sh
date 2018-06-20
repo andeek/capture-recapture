08aaa70dda9d6757faf085468cd8a31cbf9c062c
Andee Kaplan <andee.kaplan@gmail.com>
2018-06-19 22:27:06 -0400

    fix geco diag

--- code/record_linkage/run_geco_eber_diag.sh
+++ code/record_linkage/run_geco_eber_diag.sh
@@ -1,3 +1,3 @@
-screen -d -m -S rl-5-1 Rscript code/record_linkage/geco_eber_diag.R 5 1 "geco_sim"
-screen -d -m -S rl-5-3 Rscript code/record_linkage/geco_eber_diag.R 5 3 "geco_sim"
-screen -d -m -S rl-5-5 Rscript code/record_linkage/geco_eber_diag.R 5 5 "geco_sim"
+screen -d -m -S rl-5-1 Rscript code/record_linkage/geco_eber_diag.R 5 1
+screen -d -m -S rl-5-3 Rscript code/record_linkage/geco_eber_diag.R 5 3
+screen -d -m -S rl-5-5 Rscript code/record_linkage/geco_eber_diag.R 5 5
