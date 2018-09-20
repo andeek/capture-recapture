bd99f3bee39a7b4c621f9bfd11e8f8ab51418de7
Andee Kaplan <andee.kaplan@gmail.com>
2018-09-20 14:13:32 -0400

    fixed nesting

--- code/error_simulation/do_crc.R
+++ code/error_simulation/do_crc.R
@@ -14,32 +14,24 @@ load("results/error_simulation/contings.Rdata")
 # capture-recapture ----
 K <- 2^D - 1 ## from paper, number of unique capture histories
 
-error_add_pop_N <- foreach(r = seq_along(error_add_conting), .combine = rbind) %dopar% {
-  error_conting <- error_add_conting[[r]]
-  `%dopar%` <- foreach::`%dopar%`
-  foreach::foreach(i = seq_along(error_conting), .combine = rbind) %dopar% {
-    cat(paste("error: ", error_levels[r], ", iter:", i, "\r"))
-    
-    conting <- error_conting[[i]][-1,]
+error_add_pop_N <- foreach(r = seq_along(error_levels), .combine=rbind, .verbose = TRUE) %:% 
+  foreach(i = seq_len(rep), .combine=rbind, .verbose = TRUE) %dopar% {
+    conting <- error_add_conting[[r]][[i]][-1,]
     conting[, -ncol(conting)] <- as.data.frame(lapply(conting[, -ncol(conting)], function(x) as.factor(as.character(as.numeric(x)))), stringsAsFactors = TRUE)
     crc_sampler <- LCMCR::lcmCR(conting, tabular = TRUE, K = K, seed = 1234)
     pop_N <- LCMCR::lcmCR_PostSampl(crc_sampler, burnin = 100000, samples = 500, thinning = 20)
     data.frame(pop_N = pop_N, i = i, r = error_levels[r])
   }
-}
-error_remove_pop_N <- foreach(r = seq_along(error_remove_conting), .combine = rbind) %dopar% {
-  error_conting <- error_remove_conting[[r]]
-  `%dopar%` <- foreach::`%dopar%`
-  foreach::foreach(i = seq_along(error_conting), .combine = rbind) %dopar% {
-    cat(paste("error: ", error_levels[r], ", iter:", i, "\r"))
-    
-    conting <- error_conting[[i]][-1,]
+
+error_remove_pop_N <- foreach(r = seq_along(error_levels), .combine=rbind, .verbose = TRUE) %:% 
+  foreach(i = seq_len(rep), .combine=rbind, .verbose = TRUE) %dopar% {
+    conting <- error_remove_conting[[r]][[i]][-1,]
     conting[, -ncol(conting)] <- as.data.frame(lapply(conting[, -ncol(conting)], function(x) as.factor(as.character(as.numeric(x)))), stringsAsFactors = TRUE)
     crc_sampler <- LCMCR::lcmCR(conting, tabular = TRUE, K = K, seed = 1234)
     pop_N <- LCMCR::lcmCR_PostSampl(crc_sampler, burnin = 100000, samples = 500, thinning = 20)
     data.frame(pop_N = pop_N, i = i, r = error_levels[r])
   }
-}
+
 # stop parallel ----
 stopCluster(cl)
 stopImplicitCluster()
